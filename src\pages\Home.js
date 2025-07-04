95361459561eb3871147c762681bcbe75e65f317
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:44:05 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Hollywood!</h1>
+      <p>Get the latest news, movies, and celebrity updates here.</p>
+    </div>
+  );
+}
+
+export default Home;
